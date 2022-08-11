c7b0e411bab66b40192178c9d950caa450083e0c
Panagiotis Atmatzidis <atma@convalesco.org>
2022-08-11 12:20:04 +0300

    feat: enhance watch.sh script
    Increase visibility by adding datetime and `CLUSTER_NAME` to coredumps.
    This way will be easier to differentiate between different clusters.
    
    NOTE: The `CLUSTER_NAME` variable comes from the kubernetes deployment
    definition and is defined during deployment.

--- watch.sh
+++ watch.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 set -e
 
-# handle SIGTERM gracefully
+# SIGTERM propagation to child process
 _t() {
-  echo "Caught SIGTERM signal!"
+  echo "[$(date +%F %T)] Caught SIGTERM signal!"
   kill -TERM "$child" 2>/dev/null
 }
 
 trap _t SIGTERM
 
-# set node variable
+# set kubernetes related variables
 n_name="${NODE_NAME:-unknown}"
-echo "Monitoring ${LOCAL_PATH} on ${n_name}"
+c_name="${CLUSTER_NAME:-unknown}"
+
+echo "[$(date +%F %T)] Monitoring ${LOCAL_PATH} on ${n_name}"
 
 # launch inotifywait in the background
 inotifywait -m /"${LOCAL_PATH}" -e close_write | while read path action file
 do
-  aws s3 cp "${path}/${file}" "s3://${S3_BUCKET}/${n_name}.${file}"
-  echo "The file '$path' has been uploaded to 's3://${S3_BUCKET}/${n_name}.${file}'"
+  n="${S3_BUCKET}/${c_name}.${n_name}.${file}"
+  aws s3 cp "${path}/${file}" "s3://${n}"
+  echo " [$(date +%F %T)] [coredump] '${file}' has been uploaded to 's3://${n}'"
 done &
 
 # propagate bash signal to child
